809c51fce78ae87e612b8ce7cd15cf514ae04d50
DiegoSousaSilva <derickbenji2@gmail.com.br>
2022-01-14 22:39:41 -0300

    Reaproveitando codigo do formulario

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,19 +1,8 @@
 <h1>Editar o post {{$post->title}}</h1>
 
-@if ($errors->any())
-    <div>
-        <ul>
-        @foreach ($errors->all(); as $error)
-            <li>{{$error}}</li>
-        @endforeach
-        </ul>
-    </div>
-@endif
+
 
 <form action="{{route('posts.update', $post->id)}}}" method="post">
-    @csrf
     @method('put')
-    <input type="text" name="title" id="title" placeholder="Titulo" value="{{$post->title}}" >
-    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteudo">{{$post->content}}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.posts._partials.form')
 </form>
